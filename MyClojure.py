47ba61d4c6a615bd4aec2ac0fd498eab5504f0c1
KGOH <kgofhedgehogs@gmail.com>
2024-03-13 14:56:32 +0200

    Add test fails highlight

--- MyClojure.py
+++ MyClojure.py
@@ -1,5 +1,6 @@
 import sublime, sublime_plugin
 import textwrap
+import re
 cs = __import__("Clojure Sublimed")
 cs_eval = cs.cs_eval
 cs_common = cs.cs_common
@@ -89,6 +90,161 @@ class MyClojureSublimedSelectCommand(sublime_plugin.TextCommand):
         state = cs_common.get_state(self.view.window())
         letsc_start_fstr = "(ps.sc/letsc-select-start-no-mark! (quote %s))"
         state.conn.eval(self.view, self.view.sel(), transform_fn=(lambda code, **kwargs: letsc_start_fstr%code), on_finish=my_on_success_callback(self))
+
+
+    def is_enabled(self):
+        return cs_conn.ready(self.view.window())
+
+
+class ObjNode:
+    def __init__(self, node, obj, string):
+        self.node = node
+        self.obj = obj
+        self.string = string
+
+    def __eq__(self, other):
+        return str(self) == str(self)
+
+    def __hash__(self):
+        return hash(self.string)
+
+    def __str__(self):
+        return(self.string)
+
+
+def to_obj(node, string):
+    if 'token' == node.name and node.text:
+        s = node.text
+        if 'true' == s:
+            result = True
+        elif 'false' == s:
+            result = False
+        elif 'nil' == s:
+            result = None
+        elif ':' == s[0]:
+            result = s
+        elif re.fullmatch(r'[+-]?[0-9]*\.[0-9]*([eE][+-]?\d+)?', s):
+            result = float(s)
+        elif re.fullmatch(r'[+-]?[0-9]+', s):
+            result = int(s)
+        else:
+            result = string[node.start:node.end]
+    elif 'string' == node.name and node.body:
+        result = re.sub(r'\\[\\"rntfb]', cs_parser.unescape, node.body.text)
+    elif 'string' == node.name:
+        result = ''
+    elif 'source' == node.name:
+        result = [to_obj(ch, string) for ch in node.children] 
+    elif 'parens' == node.name:
+        result = tuple(to_obj(ch, string) for ch in node.body.children)
+    elif 'brackets' == node.name:
+        result = [to_obj(ch, string) for ch in node.body.children]
+    elif 'braces' == node.name and node.open.text == '#{':
+        result = {to_obj(ch, string) for ch in node.body.children}
+    elif 'braces' == node.name and node.open.text == '{':
+        result = {to_obj(k_ch, string): to_obj(v_ch, string) for k_ch, v_ch in cs_parser.partition(node.body.children, 2)} 
+    else:
+        result = string[node.start:node.end]
+    return ObjNode(obj=result, node=node, string=string[node.start:node.end])
+
+
+def regions_intersect(a_begin, a_end, b_begin, b_end): 
+    return a_begin <= b_end and a_end >= b_end
+
+
+def search_by_line(node, line_region, pred = lambda x: True, max_depth = 1000):
+    """
+    Search inside node what’s the deepest node that starts at the specified line.
+    Stops at max_depth and if pred evals to true.
+    If multiple nodes touch around pos, checks all for pred, returns the first that returns true.
+    """
+    if max_depth <= 0 or not node.children:
+        if pred(node):
+            return node
+        else:
+            return None
+    els = node.body.children if node.body else node.children
+    for child in els:
+        if (line_region.begin() <= child.start <= line_region.end()) and (pred(child)):
+            return child
+        if regions_intersect(child.start, child.end, line_region.begin(), line_region.end()):
+            if res := search_by_line(child, line_region, pred = pred, max_depth = max_depth - 1):
+                return res
+        elif line_region.end() < child.start:
+            break
+
+
+def my_test_report_callback(self):
+    def callback(eval):
+        try:
+            if "failure" == eval.status:
+                view = eval.view
+                parsed_view = cs_parser.parse_tree(view)
+
+                eval_obj = to_obj(cs_parser.parse(eval.value), eval.value).obj[0]
+
+                if reports := eval_obj.obj.get(":reports"): 
+                    if fails := reports.obj.get(":fail"):
+                        for fail in fails.obj:
+                            line = fail.obj[":line"].obj
+                            line_region = view.line(view.text_point(line-1, 0))
+
+                            found_expr = search_by_line(parsed_view, line_region)
+                            found_region = sublime.Region(found_expr.start, found_expr.end)
+
+                            highlight_region = found_region
+
+                            h_eval = cs_eval.Eval(view, highlight_region)
+                            h_value = str(fail.obj[":actual"])
+                            h_eval.update("failure", h_value, region=highlight_region)
+
+                #if errors := reports.get(":error", []):
+                #    print(len(errors), errors)
+
+
+        except Exception as e:
+            print(e)
+
+    return callback 
+
+
+clojure_sublimed_middleware_test = '''
+(ns clojure-sublimed.middleware.test
+  (:require [clojure.test]))
+
+(defn append-report! [m]
+  (when clojure.test/*report-counters*
+    (dosync 
+      (commute clojure.test/*report-counters* update-in [:reports (:type m)] (fnil conj []) m))))
+
+(defmulti report :type)
+(defmethod report :default [_])
+(defmethod report :fail [m] (append-report! m)) 
+(defmethod report :error [m] (append-report! m)) 
+'''
+
+
+# NOTE: important to be all at the same line as deftest because reports will refer to errors by line
+with_report_fstr = \
+'''(do''' \
+''' (require '[clojure-sublimed.middleware.test])''' \
+''' (let [clojure-test-report clojure.test/report]''' \
+'''  (with-bindings {#'clojure.test/report (fn [m] (clojure-test-report m) (clojure-sublimed.middleware.test/report m))}''' \
+'''    %s)))'''
+
+
+class MyClojureSublimedRunTestCommand(sublime_plugin.TextCommand):
+    def run(self, edit, run_test_xform):
+        state = cs_common.get_state(self.view.window())
+
+        state.conn.eval_status(clojure_sublimed_middleware_test, 'user')
+
+        state.conn.eval(
+            self.view,
+            self.view.sel(), 
+            transform_fn=cs_eval.format_code_fn(with_report_fstr%run_test_xform), 
+            on_finish=my_test_report_callback(self)
+        )
 
 
     def is_enabled(self):
