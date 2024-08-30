83e330d817adb339165bfea91236cba0d328aaba
KGOH <kgofhedgehogs@gmail.com>
2024-08-30 11:43:11 +0200

    Upd

--- MyClojureJackIn.py
+++ MyClojureJackIn.py
@@ -65,7 +65,7 @@ class MyClojureJackInCommand(sublime_plugin.WindowCommand):
     def run(self):
         dir = project_dir(self) 
         if os.path.isfile(os.path.join(dir,'project.clj')):
-            self.window.run_command(cmd="executor_execute_shell", args={'dir': dir, "command": '''freeport > .repl-port && echo "$(<.repl-port)" && JVM_OPTS=-Dclojure.server.repl="{:port,$(<.repl-port),:accept,clojure.core.server/repl}" lein repl :headless'''})
+            self.window.run_command(cmd="executor_execute_shell", args={'dir': dir, "command": '''freeport > .repl-port && echo "$(<.repl-port)" &&  lein repl :headless :port $(<.repl-port)'''})
             self.window.run_command(cmd="clojure_sublimed_connect_nrepl_raw", args={"address": 'auto', "timeout": 30})
         else:
             self.window.run_command(cmd="executor_execute_shell", args={'dir': dir, "command": """clojure -A:dev -M -e '(-> (clojure.core.server/start-server {:name "repl" :port 0 :accept (symbol "clojure.core.server/repl") :server-daemon false}) .getLocalPort (doto println) (->> (spit ".repl-port")))'"""})
